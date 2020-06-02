48ccfba984bce4014742a874578db82a4a9a012e
zubquzaini <zubliquzaini@gmail.com>
2020-06-02 14:31:19 +0800

    Introduction to Dart

---
+++ bin/dart_for_noob.dart
@@ -0,0 +1,21 @@
+void main(List<String> arguments) { // Entry Point
+
+  var country = 'Malaysia';
+  print(country);
+
+  num age = 13;
+  print(age);
+
+  bool isTrue = false;
+  print(isTrue);
+
+  // Concate
+  var name = 'Luke';
+  var lastName = 'Skywalker';
+
+  print ('$name ${lastName.toUpperCase()}, I am your FATHER!');
+
+  // Control Flow
+  print(name is String);
+  print(name is! String);
+}
