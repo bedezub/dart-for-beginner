336fdbef14b2e400e79e3348298ac8bff4557291
zubquzaini <zubliquzaini@gmail.com>
2020-06-08 11:55:59 +0800

    Control Flow

--- bin/dart_for_noob.dart
+++ bin/dart_for_noob.dart
@@ -2,20 +2,31 @@ void main(List<String> arguments) { // Entry Point
 
   var country = 'Malaysia';
   print(country);
-
   num age = 13;
   print(age);
-
+  
   bool isTrue = false;
   print(isTrue);
-
   // Concate
   var name = 'Luke';
   var lastName = 'Skywalker';
-
   print ('$name ${lastName.toUpperCase()}, I am your FATHER!');
-
   // Control Flow
   print(name is String);
   print(name is! String);
+  
+  doSomething();
+  print(sayHello('Quzaini'));
 }
+
+// My first dart function!!
+void doSomething() {
+  
+  print('Hello functions');
+}
+
+// => expr == { return  expression}
+String getName() => 'James Bond';
+
+// Optional parameter. Wow!
+String sayHello(String firstName, [String lastName]) => 'Mellow $firstName $lastName';
