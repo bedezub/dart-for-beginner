25b84a83052f960c003d60007307d517ca83060c
zubquzaini <zubliquzaini@gmail.com>
2020-06-08 12:20:30 +0800

    OOP Introduction

--- bin/dart_for_noob.dart
+++ bin/dart_for_noob.dart
@@ -1,22 +1,38 @@
-void main(List<String> arguments) { // Entry Point
 
-  var country = 'Malaysia';
-  print(country);
-  num age = 13;
-  print(age);
+class Microphone {
   
-  bool isTrue = false;
-  print(isTrue);
-  // Concate
-  var name = 'Luke';
-  var lastName = 'Skywalker';
-  print ('$name ${lastName.toUpperCase()}, I am your FATHER!');
-  // Control Flow
-  print(name is String);
-  print(name is! String);
+  // instance of Microphone class
+  String name;
+  String color;
+  int model;
   
-  doSomething();
-  print(sayHello('Quzaini'));
+  // Syntactic sugar constructor
+  Microphone(this.name, this.color, this.model);
+  
+  Microphone.initialize() {
+    name = 'Red';
+    model = 1212;
+  }
+
+  void turnOn() {
+    print('$name is on!');
+  }
+  
+  void turnOff() {
+    print('$name is turned off!');
+  }
+}
+void main(List<String> arguments) { // Entry Point
+  
+  var name = 'Blue';
+  var color = 'Sliver';
+  var model = 1234;
+  var mic = Microphone(name, color, model);
+  
+  var micSecond = Microphone.initialize();
+  print(micSecond);
+  mic.turnOff();
+  print(mic.model);
 }
 
 // My first dart function!!
@@ -30,3 +46,23 @@ String getName() => 'James Bond';
 
 // Optional parameter. Wow!
 String sayHello(String firstName, [String lastName]) => 'Mellow $firstName $lastName';
+
+void iLearnInFirstClass() {
+  
+  var country = 'Malaysia';
+  print(country);
+  num age = 13;
+  print(age);
+  bool isTrue = false;
+  print(isTrue);
+  // Concate
+  var name = 'Luke';
+  var lastName = 'Skywalker';
+  print ('$name ${lastName.toUpperCase()}, I am your FATHER!');
+  // Control Flow
+  print(name is String);
+  print(name is! String);
+  
+  doSomething();
+  print(sayHello('Quzaini'));
+}
